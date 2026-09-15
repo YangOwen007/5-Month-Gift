626819e7bb3efabc48396ed17b96bfc52a0ff604
YangOwen007 <yangowen321@gmail.com>
2026-09-14 22:12:02 -0700

    Add 5 Month Gift animated graph reveal

--- geometry.js
+++ geometry.js
@@ -1,1 +1,9 @@
-PLACEHOLDER_LOAD_FROM_FILE
+/* gzip-compressed geometry */
+window.__GIFT_GEOMETRY_READY__ = (async function () {
+  const b64 = "LOAD_FROM_WORKSPACE";
+  const bin = Uint8Array.from(atob(b64), (c) => c.charCodeAt(0));
+  const stream = new Blob([bin]).stream().pipeThrough(new DecompressionStream("gzip"));
+  const text = await new Response(stream).text();
+  window.GIFT_GEOMETRY = JSON.parse(text);
+  return window.GIFT_GEOMETRY;
+})();
